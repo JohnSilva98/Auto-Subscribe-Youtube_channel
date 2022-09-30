8fa1615018695143bf05aa8049816ce83c1d33f1
Jonathan Silva <jonathan.design16@outlook.com>
2022-09-30 10:51:24 -0300

    usando o selo verificado como referencia

--- Inscrever em Canal/import pyautogui.py
+++ Inscrever em Canal/import pyautogui.py
@@ -7,7 +7,7 @@ import time
 
 
 #abrir chrome
-tes = pyautogui.locateCenterOnScreen(r'chrome2.png', confidence=0.7)
+tes = pyautogui.locateCenterOnScreen(r'chrome.png', confidence=0.7)
 print("coord chrome: ",tes)
 pyautogui.moveTo(tes, duration=0.3)
 pyautogui.click()
@@ -23,7 +23,7 @@ pyautogui.hotkey('ctrl', 't')
 pyautogui.write("www.youtube.com", interval=0.050)
 pyautogui.press('enter')
 time.sleep(3.5)
-btn_pesquisar = pyautogui.locateCenterOnScreen(r"pesquisar2.png", confidence=0.9)
+btn_pesquisar = pyautogui.locateCenterOnScreen(r"pesquisar.png", confidence=0.9)
 print("coord barra: ",btn_pesquisar)
 pyautogui.moveTo(btn_pesquisar, duration=0.3)
 pyautogui.click()
@@ -39,7 +39,7 @@ pyautogui.click()
 time.sleep(2)
 
 #se inscrever
-btn_inscrever = pyautogui.locateCenterOnScreen(r"inscrever2.png", confidence=0.9)
+btn_inscrever = pyautogui.locateCenterOnScreen(r"inscrever.png", confidence=0.9)
 print("coord inscrever:",btn_inscrever)
 pyautogui.moveTo(btn_inscrever, duration=0.3)
 pyautogui.click()
